02dd582477eb002b6d439d45dce7f8f316f03e9f
unknown <bubbastinx@gmail.com>
2018-03-23 14:23:20 -0600

    CODE CHANGE### change the buttons to load dynamically from an object array. Load Buttons work at this point."

--- Assets/javaScript.js
+++ Assets/javaScript.js
@@ -1,34 +1,68 @@
 'strict'
 
-// "
+let giphyButtonObj = {
+
+    topics: ["Dougie", "Cabbage Patch","Running Man","Jit","Milly Rock", "Roger Rabbit Dance", "Humpty Dance", "Wobble"],
+    searchButtonArray: [],
+    loadTopicButtons: function (){
+        for (let t = 0; t < giphyButtonObj.topics.length; t++){
+            console.log(t);
+            let topicButtons = $("<button>");
+            topicButtons.addClass("topics");
+            topicButtons.attr("data-person", giphyButtonObj.topics[t]);
+            //make sure to ask a question about this line below to double check your understanding on how its working.
+            topicButtons.append(giphyButtonObj.topics[t]);
+            $(".gifButton").append(topicButtons)
+        }
+        
+
+    },
+};
+
+$(document).ready(giphyButtonObj.loadTopicButtons());
 
 
 
-$(".btn").on("click", function(e) {
-    console.log("data-person");
-    let person = $(this).attr("data-person");
-    let queryUrl ="https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
 
-    $.ajax({
-        url: queryUrl,
-         method: "Get"
-    })
+// $(".btn").on("click", function(e) {
+//     //could I use another data name, and would it work the same "data-topic, data-foobar" abd as long as I reference data-"name" I will be able to exract information. 
+//     console.log("data-person");
+//     let person = $(this).attr("data-person");
+//     let queryUrl ="https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
 
-.then(function(response) {
-    console.log(response);
+//     $.ajax({
+//         url: queryUrl,
+//          method: "Get"
+//     })
+
+// .then(function(response) {
+//     console.log(response);
     
-    let results = response.data;
-    for ( let i = 0; i < results.length; i++){
-        let newDiv = $(".gifHolder");
-        let ratings = results[i].rating;
-        let gifDiv = $("<div>").addClass("TheGifDiv");
-        var para = $("<p>").text("Ratings:" + ratings);
-        let gifImage = $("<img>");
-        gifImage.attr("src", results[i].images.fixed_height.url);
-        newDiv.append(results);
-        newDiv.append(para);
-        newDiv.append(gifImage);
-        newDiv.append(gifDiv);
-    }
-})
-});
+//     let results = response.data;
+//     for ( let i = 0; i < results.length; i++){
+//         let newDiv = $(".gifHolder");
+//         let title = results[i].title;
+//         let gifDiv = $("<div>").addClass("TheGifDiv");
+//         var para = $("<p>").html("<strong>" + title + "</strong>");
+//         let gifImage = $("<img>");
+//         gifImage.attr("src", results[i].images.fixed_height.url);
+//         newDiv.prepend(results);
+//         newDiv.prepend(para);
+//         newDiv.prepend(gifImage);
+//         newDiv.prepend(gifDiv);
+//     }
+// });
+
+// $(".gifHolder").on("click", function(){
+//     //ask for a walkthrough on this information within the object, for memory purposes. 
+//     let state = $(this).attr("data-state");
+//     if (state == "still") {
+//         let animateImage = $(this).attr("data-animate");
+//         $(this).attr("src", animateImage);
+//         $(this).attr("data-state", "animate");
+//          } else {
+//              $(this).attr("src",$(this).attr("data-still"));
+//              $(this).attr("data-state", "still")
+//          }
+// })
+// });
