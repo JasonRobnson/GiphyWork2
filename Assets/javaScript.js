cd390c44caabcf3e7b763f3addaf81b44ca6f1c2
unknown <bubbastinx@gmail.com>
2018-03-23 21:08:50 -0600

    Buttons still hit query, go buttons fires, and captures text from the search box

--- Assets/javaScript.js
+++ Assets/javaScript.js
@@ -1,12 +1,17 @@
 'strict'
 
+
+
+let person = $(this).attr("data-person");
+
+let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
+
 let giphyButtonObj = {
 
-    topics: ["Dougie", "Cabbage Patch","Running Man","Jit","Milly Rock", "Roger Rabbit Dance", "Humpty Dance", "Wobble"],
+    topics: ["Teach me how to Dougie", "Cabbage Patch","Running Man Dance","Shmoney Dance","Milly Rock", "Nae Nae Dance", "The sprinkler dance","The Carlton Dance"],
     searchButtonArray: [],
     loadTopicButtons: function (){
         for (let t = 0; t < giphyButtonObj.topics.length; t++){
-            console.log(t);
             let topicButtons = $("<button>");
             topicButtons.addClass("topics");
             topicButtons.attr("data-person", giphyButtonObj.topics[t]);
@@ -19,66 +24,88 @@ let giphyButtonObj = {
     loadSearchTopcis: function(){
 
     },
-    queryTopic: function(){
-        $.ajax({
-            url: queryUrl,
-            method: "Get"
-        })
-    .then(function(response){
-        console.log(response);
-    })
+    // queryTopic: function(){
+    //     let person = $(this).attr("data-person");
 
-    },
+    //     let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
+
+    //     $(".topics").on("click", function(){
+    //         $.ajax({
+    //             url: queryUrl,
+    //             method: "Get"
+    //         })
+    //         .then(function(response){
+    //             //why are we setting results to response.data versus just using response.data the rest of the way? Is it to make it easier not to mess up typing it? or tracking response?
+    //             let results = response.data;
+    //             for( let r = 0; 0 <= response.data.length; r++) {
+    //                     let newDiv = $(".gifHolder");
+    //                     let title = results[r].title;
+    //                     let gifDiv = $("<div>").addClass("TheGifDiv");
+    //                     let para = $("<p>").html("<strong>" + title + "</strong>");
+    //                     let gifImage = $("<img>");
+    //                     gifImage.attr("src", results[r].images.fixed_height.url);
+    //                     newDiv.prepend(results);
+    //                     newDiv.prepend(para);
+    //                     newDiv.prepend(gifImage);
+    //                     newDiv.prepend(gifDiv);
+    //             }
+        
+    //         })
+
+
+    //     })
+    
+
+    // },
 
 };
 
-let person = $(this).attr("data-person");
-
-let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
 
 $(document).ready(giphyButtonObj.loadTopicButtons());
+// giphyButtonObj.queryTopic();
+
+// $(".topics").on("click", function(e) {
+//     console.log("The click works!");
+//     // giphyButtonObj.queryTopic();
+// });         
 
 $(".topics").on("click", function(e) {
-    console.log("The click works!");
-    giphyButtonObj.queryTopic();
+    //could I use another data name, and would it work the same "data-topic, data-foobar" abd as long as I reference data-"name" I will be able to exract information. 
+    console.log("data-person");
+    let person = $(this).attr("data-person");
+    let queryUrl ="https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
+
+    $.ajax({
+        url: queryUrl,
+         method: "Get"
+    })
+
+.then(function(response) {
+    console.log(response);
+    
+    let results = response.data;
+    for ( let i = 0; i < results.length; i++){
+        let newDiv = $(".gifHolder");
+        let title = results[i].title;
+        let gifDiv = $("<div>").addClass("TheGifDiv");
+        var para = $("<p>").html("<strong>" + title + "</strong>");
+        let gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height.url);
+        newDiv.prepend(results);
+        newDiv.prepend(para);
+        newDiv.prepend(gifImage);
+        newDiv.prepend(gifDiv);
+    }
+})
 });
 
-//Click function on dynamic loaded buttons
+// Search box function
 
+$("#searchButton").on("click", function (){
+    let searchValue = $("#searchBox").val();
+    console.log(searchValue);
+})
 
-
-
-
-
-
-// $(".btn").on("click", function(e) {
-//     //could I use another data name, and would it work the same "data-topic, data-foobar" abd as long as I reference data-"name" I will be able to exract information. 
-//     console.log("data-person");
-//     let person = $(this).attr("data-person");
-//     let queryUrl ="https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
-
-//     $.ajax({
-//         url: queryUrl,
-//          method: "Get"
-//     })
-
-// .then(function(response) {
-//     console.log(response);
-    
-//     let results = response.data;
-//     for ( let i = 0; i < results.length; i++){
-//         let newDiv = $(".gifHolder");
-//         let title = results[i].title;
-//         let gifDiv = $("<div>").addClass("TheGifDiv");
-//         var para = $("<p>").html("<strong>" + title + "</strong>");
-//         let gifImage = $("<img>");
-//         gifImage.attr("src", results[i].images.fixed_height.url);
-//         newDiv.prepend(results);
-//         newDiv.prepend(para);
-//         newDiv.prepend(gifImage);
-//         newDiv.prepend(gifDiv);
-//     }
-// });
 
 // $(".gifHolder").on("click", function(){
 //     //ask for a walkthrough on this information within the object, for memory purposes. 
