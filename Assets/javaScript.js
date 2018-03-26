c8f34de02eaf0d2afb586ac6ab3e95bcbcdb7359
unknown <bubbastinx@gmail.com>
2018-03-26 11:29:26 -0600

    added .empty() to the topic click event listener to empty div upon new click topic

--- Assets/javaScript.js
+++ Assets/javaScript.js
@@ -1,6 +1,6 @@
 'strict'
 
-
+let gifHolder = $(".gifHolder");
 
 let person = $(this).attr("data-person");
 
@@ -15,63 +15,22 @@ let giphyButtonObj = {
             let topicButtons = $("<button>");
             topicButtons.addClass("topics");
             topicButtons.attr("data-person", giphyButtonObj.topics[t]);
-            //make sure to ask a question about this line below to double check your understanding on how its working.
+            //make sure to ask a question about this line below to double check your understanding on how its working?
             topicButtons.append(giphyButtonObj.topics[t]);
             $(".gifButton").append(topicButtons);
     
         }
     },
-    loadSearchTopcis: function(){
-
-    },
-    // queryTopic: function(){
-    //     let person = $(this).attr("data-person");
-
-    //     let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
-
-    //     $(".topics").on("click", function(){
-    //         $.ajax({
-    //             url: queryUrl,
-    //             method: "Get"
-    //         })
-    //         .then(function(response){
-    //             //why are we setting results to response.data versus just using response.data the rest of the way? Is it to make it easier not to mess up typing it? or tracking response?
-    //             let results = response.data;
-    //             for( let r = 0; 0 <= response.data.length; r++) {
-    //                     let newDiv = $(".gifHolder");
-    //                     let title = results[r].title;
-    //                     let gifDiv = $("<div>").addClass("TheGifDiv");
-    //                     let para = $("<p>").html("<strong>" + title + "</strong>");
-    //                     let gifImage = $("<img>");
-    //                     gifImage.attr("src", results[r].images.fixed_height.url);
-    //                     newDiv.prepend(results);
-    //                     newDiv.prepend(para);
-    //                     newDiv.prepend(gifImage);
-    //                     newDiv.prepend(gifDiv);
-    //             }
-        
-    //         })
-
-
-    //     })
-    
-
-    // },
 
 };
 
 
 $(document).ready(giphyButtonObj.loadTopicButtons());
-// giphyButtonObj.queryTopic();
-
-// $(".topics").on("click", function(e) {
-//     console.log("The click works!");
-//     // giphyButtonObj.queryTopic();
-// });         
 
 $(".topics").on("click", function(e) {
-    //could I use another data name, and would it work the same "data-topic, data-foobar" abd as long as I reference data-"name" I will be able to exract information. 
+    //could I use another data name, and would it work the same "data-topic, data-foobar" abd as long as I reference data-"name" I will be able to exract information? 
     console.log("data-person");
+    gifHolder.empty();
     let person = $(this).attr("data-person");
     let queryUrl ="https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
 
@@ -118,7 +77,7 @@ $("#searchButton").on("click", function (){
            let para = $("<p>").html("<strong>" + title + "</strong>");
            let gifImage = $("<img>");
            gifImage = $("<img>");
-           gifImage.attr("src", results[i].images.fixed_height.url);
+           gifImage.attr("src", results[i].images.original_still);
            newDiv.prepend(results);
            newDiv.prepend(para);
            newDiv.prepend(gifImage);
@@ -129,7 +88,6 @@ $("#searchButton").on("click", function (){
        searchButton.addClass("topics");
        searchButton.attr("data-person", searchValue);
        console.log(searchValue);
-        //make sure to ask a question about this line below to double check your understanding on how its working.
         searchButton.append(searchValue);
         $(".gifButton").append(searchButton);
     })
@@ -139,16 +97,16 @@ $("#searchButton").on("click", function (){
 
 
 
-// $(".gifHolder").on("click", function(){
-//     //ask for a walkthrough on this information within the object, for memory purposes. 
-//     let state = $(this).attr("data-state");
-//     if (state == "still") {
-//         let animateImage = $(this).attr("data-animate");
-//         $(this).attr("src", animateImage);
-//         $(this).attr("data-state", "animate");
-//          } else {
-//              $(this).attr("src",$(this).attr("data-still"));
-//              $(this).attr("data-state", "still")
-//          }
-// })
-// });
+$(".gifHolder").on("click", function(){
+    console.log("You clicked the GifDiv")
+    //ask for a walkthrough on this information within the object, for memory purposes. 
+    let state = $(this).attr("data-state");
+    if (state === "still") {
+        let animateURL = $(this).attr("data-animate");
+        $(this).attr("src", animateURL);
+        $(this).attr("data-state", "animate");
+         } else if (state ==="animate") {
+             $(this).attr("src",$(this).attr("data-still"));
+             $(this).attr("data-state", "still")
+         }
+});
